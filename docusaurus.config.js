c498e57bc89b4a3b376a452747b3c297c60aa55d
rianllauo <rianllauo@gmail.com>
2023-09-09 22:10:56 +0700

    update tite

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 // /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Creatif Stuido",
+  title: "Creatif Studio Docs",
   tagline: "Dinosaurs are cool",
   favicon: "img/favicon.ico",
 
